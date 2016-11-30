b91d69eac61e4b35099fa7d63f27337142e98e21
chaluvarajups <chaluvarajups@gmail.com>
2016-11-30 21:28:13 +0530

    remote chnage

--- GitProj/src/class1.java
+++ GitProj/src/class1.java
@@ -3,6 +3,7 @@ public class class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Practice well");		
-		System.out.println("Changes at local");
+		System.out.println("Change done at local");
+		System.out.println("Change done at remote");
 	}
 }
