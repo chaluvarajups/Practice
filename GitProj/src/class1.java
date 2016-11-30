fa55a17e92754dc19c3aea6c5cdce398128e7c2c
chaluvaraju PS <chaluvarajups@gmail.com>
2016-11-30 21:22:36 +0530

    Local changes

--- GitProj/src/class1.java
+++ GitProj/src/class1.java
@@ -2,8 +2,7 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		System.out.println("Practice well");
-
+		System.out.println("Practice well");		
+		System.out.println("Changes at local");
 	}
-
 }
